d8dca6440d8773a4ef6b6d9e099d4f55f89adf3d
Linfeng Zhao <zhao1480925128@gmail.com>
2018-10-13 22:25:27 +0800

    Study the Third Chapter about Python

---
+++ ThirdChapter/ConstellationPrint.py
@@ -0,0 +1,4 @@
+# Constellation Print
+for i in range(12):
+    print(chr(9800 + i), end="    ")
+
---
+++ ThirdChapter/TextProBarV2.py
@@ -0,0 +1,6 @@
+# Text Pro Bar V2
+
+import time
+for i in range(101):
+    print("\r{:3}%".format(i), end="")
+    time.sleep(0.2)
