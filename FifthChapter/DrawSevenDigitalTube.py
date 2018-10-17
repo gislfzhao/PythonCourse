26fea63a5b7d0a45a2e7ab867467b2bcf6c3636a
Linfeng Zhao <zhao1480925128@gmail.com>
2018-10-17 22:53:45 +0800

    Complete drawing the Clock, and modify the drawing of digital tubes.

--- FifthChapter/DrawSevenDigitalTube.py
+++ FifthChapter/DrawSevenDigitalTube.py
@@ -30,16 +30,30 @@ def draw_digit(digit):  # draw the digit
     # prepare to draw the next digit
     turtle.left(180)
     turtle.penup()
-    turtle.forward(20)
+    turtle.forward(10)
 
 
 def draw_date(date):    # draw the digits of date
     for i in date:
-        draw_digit(i)
+        if i == '-':
+            turtle.forward(10)
+            turtle.write('年', False, 'left', ("Arial", 36, "normal"))
+            turtle.pencolor("green")
+            turtle.forward(20)
+        elif i == '+':
+            turtle.forward(10)
+            turtle.write('月', False, 'left', ("Arial", 36, "normal"))
+            turtle.pencolor("blue")
+            turtle.forward(20)
+        elif i == '=':
+            turtle.forward(10)
+            turtle.write('日', False, 'left', ("Arial", 36, "normal"))
+        else:
+            draw_digit(i)
 
 
 def get_localtime():    # get the string of the local time
-    return time.strftime("%Y%m%d%H%M%S", time.localtime())
+    return time.strftime("%Y-%m+%d=", time.localtime())
 
 
 def main():
@@ -47,7 +61,10 @@ def main():
     turtle.penup()
     turtle.forward(-300)
     turtle.pensize(4)
+    turtle.pencolor("red")
     turtle.hideturtle()
+    turtle.tracer(False)     # 关闭绘画追踪，可以用于加速绘画复杂图形
+    draw_date(get_localtime())
     turtle.done()
 
 
